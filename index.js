3004ac70b7e1c3a960ed969453c520a8bf751400
Aaron Morse <morse.aaron1985@gmail.com>
2025-04-21 09:57:36 -0700

    Basic expres server added.

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
+
+// Get routes
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "contact.html"));
+});
