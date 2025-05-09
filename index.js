7967418be530e765f68d6d649b03ecb46d4d1dc3
Aaron Morse <morse.aaron1985@gmail.com>
2025-05-08 21:30:36 -0700

    Added the functionality to the form for multimedia and I am using the express file upload package to read the image in the req body and updated the /posts/store route to add the image to the db with the updated model and the post.ejs view uses the image that gets stored in public/img as the header image.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const BlogPost = require("./models/BlogPost");
+const fileUpload = require("express-fileupload");
 
 mongoose.connect("mongodb://localhost/my_database");
 
@@ -14,6 +15,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
@@ -48,14 +50,15 @@ app.get("/posts/new", (req, res) => {
 });
 
 //Post routes
-app.post("/posts/store", async (req, res) => {
-  console.log(req.body);
-  try {
-    const blogpost = await BlogPost.create(req.body);
-    if (blogpost) {
-      res.redirect("/");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+app.post("/posts/store", (req, res) => {
+  let image = req.files.image;
+  const imagePath = path.resolve(__dirname, "public/img", image.name);
+
+  image.mv(imagePath, async (error) => {
+    await BlogPost.create({
+      ...req.body,
+      image: `/img/${image.name}`,
+    });
+    res.redirect("/");
+  });
 });
